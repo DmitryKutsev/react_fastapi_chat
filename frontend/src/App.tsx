5bf8c97c21015faf8bab62cdd562ab816e9de7fe
dima_in_the_forest <31938190+DmitryKutsev@users.noreply.github.com>
2024-01-27 17:58:24 +0000

    Started project, playing around React

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import Controller from "./components/Controller";
+
+function App() {
+  return (
+      <div className="App">
+        <div className="text-6xl">Hello world!</div>
+        <Controller />
+      </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Controller.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Controller() {
+  return (
+    <div>Hello Controller</div>
+  )
+}
+
+export default Controller
