d6241effc64a0156b059351b6382557c5a9578e2
dima_in_the_forest <dmitrykucev@mail.ru>
2024-02-11 21:40:24 +0100

    Added small prompts and request to chatgpt

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
-from utils.openai_requests import audio_to_text
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from utils.openai_requests import audio_to_text, get_chat_response
+
+origins = [
+    "http://localhost:5713",
+    "http://localhost:5714",
+    "http://localhost:3000",
+]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 async def healthcheck():
@@ -13,3 +29,7 @@ async def get_audio():
     with open("my_record.mp3", "rb") as record:
         return {"text": audio_to_text(record)}
 
+
+@app.post("/responce_text")
+async def responce_text(request_text: str):
+     return JSONResponse(content={"response": get_chat_response(request_text)})
--- backend/utils/openai_requests.py
+++ backend/utils/openai_requests.py
@@ -14,10 +14,34 @@ client = openai.OpenAI()
 def audio_to_text(audio_file_path):
     try:
         transcript = client.audio.transcriptions.create(
-            model="whisper-1", 
+            model="whisper-1",
             file=audio_file_path,
-            )
+        )
         return transcript.text
     except Exception as err:
         logger.error(f"Error in audio_to_text: {err}")
         return
+
+
+def get_chat_response(message_input):
+
+    # messages = get_recent_messages()
+    user_message = {
+        "role": "user",
+        "content": f"{message_input} ",
+    }
+    # messages.append(user_message)
+    # print(messages)
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": f"You are a helpful assistant translator. You knows three languages:"  # noqa: F541
+                           f"Russian, Dutch and English. Provide traslation of the given phrase with all languages you know besides the language of the request", # noqa: F541
+            },
+            user_message,
+        ],
+    )
+    return response.choices[0].message.content
